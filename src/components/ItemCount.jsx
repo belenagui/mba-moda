2fd5b0395ec43701fb238b6d2e33fd9453f72180
Belen Aguirre <aguirrebelen@frba.utn.edu.ar>
2023-09-09 23:02:55 -0300

    desarrollo de itemcount

--- src/components/ItemCount.jsx
+++ src/components/ItemCount.jsx
@@ -1,23 +1,35 @@
+import { Center, Flex, Box } from '@chakra-ui/react'
 import React, { useState } from 'react'
 
 const ItemCount = () => {
 
     const [contador, setContador] = useState(0)
     const onAdd = () =>{
-        alert("Gracias!! Acabas de comprar: " + contador + "Productos")
+        alert("Gracias!! Acabas de comprar: " + contador + " Productos")
         setContador(0)
     }
 
     return (
     <>
       <div className='center'>
+      <Center>
+        <Flex>
+            <Box px="2">
+                <button className='md-2' onClick={() => contador < 10? setContador(contador + 1) : setContador(10)}>+</button>
+            </Box>
+            <Box px="2">
+                <p className='md-2'>{contador}</p>
+            </Box>
+            <Box px="2">
+                <button className='md-2' onClick={() => contador < 10? setContador(contador - 1) : setContador(0)}>-</button>
+            </Box>
+            <Box px="2">
+                <button className='md-2' onClick={onAdd}> Comprar </button>
+            </Box>
 
-
-
-        
+        </Flex>
+      </Center>
       </div>
-      <p>{contador}</p>
-      <button onClick={onAdd}>Mostrar Cantidad</button>
     </>
   )
 }
